d89e7a5f7288c34fdf339302cc6dc037b052f8ab
Udomomo <batzuma@yahoo.co.jp>
2018-04-30 16:28:05 +0900

    関数をアロー関数に変更

--- server.js
+++ server.js
@@ -2,33 +2,33 @@ let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/client.js', function(req, res){
+app.get('/client.js', (req, res) => {
   res.sendFile(__dirname + '/client.js');
 });
 
-io.on('connection', function(socket){
-  socket.on('new_entry', function(name){
+io.on('connection', (socket) => {
   let name;
+  socket.on('new_entry', (name) => {
     socket.broadcast.emit('chat_message', name + ' has joined.');
   });
   
-  socket.on('new_message', function(data){
+  socket.on('new_message', (data) => {
     name = data.name;
     let msg = data.msg;
     io.emit('chat_message', name + ": " + msg);
   });
 
-  socket.on('disconnect', function(){
+  socket.on('disconnect', () => {
     if (name != undefined) {
       io.emit('chat_message', name + " has left.");
     }
   });
 });
 
-http.listen(3000, function(){
+http.listen(3000, () => {
   console.log('listening on *:3000');
 });
